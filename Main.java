c4cdcc84696b720f04cb82b63e4ac25367fa056f
sikandar976833 <sikandarcode2024@gmail.com>
2025-03-09 16:58:28 +0530

    intial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+//22. Remove duplicates from an array.
+
+package Wissen;
+
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String args[])
+    {
+    
+    
+    System.out.println("Welcome to jenkins ");
+            
+    
+}
+}
